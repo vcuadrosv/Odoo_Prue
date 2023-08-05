bf4bcb390f37a90b8a742b77df28e3da3ccbaa81
vcuadrosv <valery.05@hotmail.com>
2023-08-05 13:15:41 -0500

    feat: cambios de odoo

--- prueba1/models/models.py
+++ prueba1/models/models.py
@@ -1,7 +1,55 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields
-from odoo.exceptions import UserError
+
+
+class ficha_empleados(models.Model):
+    _name = 'ficha.empleados'
+    _description = 'empleados'
+
+
+    STATE_SELECTION = [
+        ('activo', 'Activo'),
+        ('no_activo', 'No Activo'),
+    ]
+
+    SEX_SELECTION = [
+        ('masculino', 'Masculino'),
+        ('femenino', 'Femenino'),
+        ('otro', 'Otro'),
+    ]
+
+    JOR_SELECTION = [
+        ('Jornada_Completa', 'Jornada Completa'),
+        ('Media_Jornada', 'Media_Jornada'),
+        ('No_tiene', 'No tiene'),
+    ]
+
+    Foto = fields.Image(required=True)
+    nombre = fields.Char (string = 'Primer Nombre', required=True)
+    nombre2 = fields.Char (string = 'Segundo Nombre')
+    apellido = fields.Char (string = 'Primer Apellido',required=True)
+    apellido2 = fields.Char (string = 'Segundo Apellido')
+    tipo = fields.Selection([('CC', 'Cedula de Ciudadania'), ('NIT', 'Numero de identificacion tributaria'), ('RC', 'Registro Civil'),
+                            ('TI', 'Tarjeta de Identidad'),('CE', 'Cedula de extranjeria'),('PA', 'Pasaporte')], string='Tipo', required=True, help="Seleccione el tipo de documento:\n"
+                                                                                                                                                    "- CC: Cedula de Ciudadania\n"
+                                                                                                                                                    "- NIT: Numero de identificacion tributaria\n"
+                                                                                                                                                    "- RC: Registro Civil\n"
+                                                                                                                                                    "- TI: Tarjeta de Identidad\n"
+                                                                                                                                                    "- CE: Cedula de extranjeria\n"
+                                                                                                                                                    "- PA: Pasaporte")
+    numero_cedula = fields.Char(string="Número de Cédula")
+    fecha_nacimiento = fields.Date(string='Fecha de Nacimiento', required=True)
+    cargo = fields.Char (string = 'Cargo')
+    celular = fields.Integer(string="Celular")
+    direccion = fields.Char(string='Dirección', required=True)
+    tipo_sangre = fields.Char(string='Tipo de Sangre')
+    email = fields.Char(string='Correo electrónico', required=True)
+    estado = fields.Selection(STATE_SELECTION, string='Estado', default='activo', required=True)
+    sexo = fields.Selection(SEX_SELECTION, string='Sexo', required=True)
+    jornada = fields.Selection(JOR_SELECTION, string='Jornada', required=True)
+
+
 
 class Visit(models.Model):
     _name = 'prueba1.visit'
